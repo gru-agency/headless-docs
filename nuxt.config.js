636b8c3ecd7e05c93c5282e4348f24f18cdee43a
Raymond Chai <csechuan@gmail.com>
2021-06-28 19:34:57 +0800

    build: change hosting port to 3010

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,10 @@ export default theme({
   ssr: false,
   target: "static",
 
+  server: {
+    port: 3010,
+  },
+
   docs: { primaryColor: "#E24F55" },
   loading: { color: "#00CD81" },
   i18n: {},
