174f8c0a960716fcde397335d11c9dc0827d33a6
Raymond Chai <csechuan@gmail.com>
2021-06-28 19:32:08 +0800

    build: change to spa

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,11 @@
-import theme from '@nuxt/content-theme-docs'
+import theme from "@nuxt/content-theme-docs";
 
 export default theme({
-  docs: {
-    primaryColor: '#E24F55'
-  }
-})
+  ssr: false,
+  target: "static",
+
+  docs: { primaryColor: "#E24F55" },
+  loading: { color: "#00CD81" },
+  i18n: {},
+  buildmodules: [],
+});
